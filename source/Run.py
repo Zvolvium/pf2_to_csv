f427bd4e58cecfa003b934842e3efa056890742b
Zvolvium <motschke61@gmail.com>
2024-03-03 22:37:14 -0600

    Class constructor modification (construction with or without file_path argument).
    Many other changes including method/variable name changes, method/print generalization.

--- source/Run.py
+++ source/Run.py
@@ -11,7 +11,7 @@
         None
 
     Author:
-        run.py - Mason Motschke
+        run.py - Mason Motschke (Zvolvium)
 """
 from csv import DictWriter
 from time import sleep
@@ -20,34 +20,40 @@ from tkinter import filedialog
 
 
 class PF2CsvConverter:
-    def __init__(self):
-        self.win = Tk()
-        self.win.geometry("50x25")
-        Label(self.win, text="File Dialog", font='Arial 16 bold').pack(pady=15)
+    def __init__(self, file_path=None):
+        if file_path is None:
+            self.win = Tk()
+            self.win.geometry("50x25")
+            Label(self.win, text="File Dialog", font='Arial 16 bold').pack(pady=15)
 
-        filePath = filedialog.askopenfilename(title="Open a PF2 file",
-                                              filetypes=(("pf2 files", "*.pf2"), ("all files", "*.*")))
+            file_path = filedialog.askopenfilename(title="Open a PF2 file",
+                                                   filetypes=(("pf2 files", "*.pf2"), ("all files", "*.*")))
+
         try:
-            self.check_filePath(filePath)
+            self.check_file_extension(file_path)
         except ValueError as ve:
-            print(f"\n!!EXCEPTION: {ve}\n    -> {filePath}")
+            print(f"\n!!EXCEPTION: {ve}\n    -> {file_path}")
             return
 
-        self.pf2_file = filePath
-        self.csv_file = self.getCsvFileName(filePath)
+        self.pf2_file = file_path
+        self.csv_file = self.get_new_filename(file_path)
 
-    def openStream(self, file, mode='r'):
+    @staticmethod
+    def open_stream(file, mode='r'):
         return open(str(file), str(mode))
 
-    def closeStream(self, stream):
+    @staticmethod
+    def close_stream(stream):
         stream.close()
 
-    def check_filePath(self, path):
+    @staticmethod
+    def check_file_extension(path, extension='pf2'):
         temp = path.strip().split('.')
-        if temp[-1] != 'pf2':
-            raise ValueError('File Selected is not a PF2 file!')
+        if temp[-1] != extension:
+            raise ValueError(f'File Selected is not a {extension.upper()} file!')
 
-    def buildRow(self, headers, data):
+    @staticmethod
+    def build_row(headers, data):
         row = {}
         i = 0
 
@@ -57,42 +63,45 @@ class PF2CsvConverter:
 
         return row
 
-    def getCsvFileName(self, pf2_filename):
-        temp = pf2_filename.strip().split('.')
-        temp.append('csv')
-        temp.pop(-2)
+    @staticmethod
+    def get_new_filename(path, extension='csv'):
+        temp = path.strip().split('.')
+        temp.pop(-1)
+        temp.append(extension)
         return '.'.join(temp)
 
-    def getFieldNames(self, fieldNameLine):
-        return fieldNameLine[7:-1].strip().split(', ')
+    @staticmethod
+    def get_field_names(field_name_line):
+        return field_name_line[7:-1].strip().split(', ')
 
-    def getPF2Data(self, stream):
+    def get_file_data(self, stream):
         data = []
+        field_name_line = ''
         i = 0
 
         for line in stream:
             if line.startswith('Data:'):
-                fieldNameLine = line.strip()
+                field_name_line = line.strip()
             elif line[0].isnumeric():
                 line = line.strip().split(', ')
                 data.append(line)
                 i += 1
 
-        self.closeStream(stream)
-        return [data, i, self.getFieldNames(fieldNameLine)]
+        self.close_stream(stream)
+        return [data, i, self.get_field_names(field_name_line)]
 
     def run(self):
         try:
-            tuple = self.getPF2Data(self.openStream(self.pf2_file))
+            Tuple = self.get_file_data(self.open_stream(self.pf2_file))
 
             with open(self.csv_file, 'w', newline='') as converted_file:
-                writer = DictWriter(converted_file, fieldnames=tuple[2])
+                writer = DictWriter(converted_file, fieldnames=Tuple[2])
                 writer.writeheader()
 
-                for j in range(0, tuple[1]):
-                    writer.writerow(self.buildRow(tuple[2], tuple[0][j]))
+                for j in range(0, Tuple[1]):
+                    writer.writerow(self.build_row(Tuple[2], Tuple[0][j]))
 
-            self.closeStream(converted_file)
+            self.close_stream(converted_file)
             print("\nSuccessfully converted the PF2 file to CSV")
             print("\n -- Application closing in 5 seconds...")
             sleep(5)
@@ -103,5 +112,17 @@ class PF2CsvConverter:
 
 
 if __name__ == "__main__":
-    PF2CsvConverter = PF2CsvConverter()
-    PF2CsvConverter.run()
+    # Without argument
+    PF2CsvConverter1 = PF2CsvConverter()
+    PF2CsvConverter1.run()
+
+    # With argument
+    # win = Tk()
+    # win.geometry("50x25")
+    # Label(win, text="File Dialog", font='Arial 16 bold').pack(pady=15)
+    #
+    # file_path = filedialog.askopenfilename(title="Open a PF2 file",
+    #                                        filetypes=(("pf2 files", "*.pf2"), ("all files", "*.*")))
+    #
+    # PF2CsvConverter2 = PF2CsvConverter(file_path)
+    # PF2CsvConverter2.run()
